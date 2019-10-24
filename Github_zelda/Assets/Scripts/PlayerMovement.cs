1eee4666aa3d345a4173f734275e60742061e17c
Angel <padilla.angel88@gmail.com>
2019-10-23 19:17:15 -0500

    Add Sprites
    Animatios
    Basic movement

---
+++ Github_zelda/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+
+        position.x = Input.GetAxisRaw("Horizontal");
+        position.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+
+        rb.MovePosition(rb.position + position * moveSpeed * Time.fixedDeltaTime);
+    }
+}
